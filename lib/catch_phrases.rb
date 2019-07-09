3f763a8524794451004265ce9d3ce2f516af2ffb
lite-post-0822 <amberlowe40@gmail.com>
2019-07-09 00:32:49 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,6 +1,10 @@
 def mario
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts phrase
+  phrase = "its's-a-me, Mario"
+  expect{mario}.to_stdout
+  
+  end
 end
 
 def toadstool
